a66d2a6b46f2558fc1771f613cdd4ccfed45dcf9
190834 <190834@hkis.edu.hk>
2018-08-23 15:42:47 +0800

    Version 1
    Hello

---
+++ src/Return.java
@@ -0,0 +1,15 @@
+public class Return {
+
+	public static void main(String[] args) {
+		System.out.println(sum1());
+
+	}
+
+	public static int sum1() {
+		int x = 4;
+		int y= 5;
+
+		return (x+y);
+
+	}
+}
---
+++ src/Return2.java
@@ -0,0 +1,13 @@
+public class Return2 {
+
+	public static void main(String[] args) {
+		System.out.println(sum3(3,5));
+
+	}
+
+	public static int sum3(int a, int b) {
+	
+		return (a+b);
+
+	}
+}
